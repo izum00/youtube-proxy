07e4d3b26e9c8fda53aca8e4c4b17bf02ccfce46
izum00 <karisute14@gmail.com>
2024-11-17 13:02:58 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const playApi = require('./api/play');
 const searchApi = require('./api/search');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 7860;
 
 app.get('/', (req, res) =>
   res.json({
